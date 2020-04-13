495bd6a6d9dba0aabf291f8e8866defd4527df2d
Krzysztof Burghardt <krzysztof@burghardt.pl>
2020-04-13 21:48:16 +0200

    Run tests as a suite to have total count of test cases.

--- tests/runtests.bash
+++ tests/runtests.bash
@@ -2,7 +2,5 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-for TEST in *.bats
-do
-    env PATH=".:/usr/local/libexec/bats-core:$PATH" ./${TEST}
-done
+env PATH=".:/usr/local/libexec/bats-core:$PATH" \
+    bats --tap .
